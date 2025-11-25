2047bdc5685b4c9aa94c2445f89b7926814bfbbc
KT <keni232127@gmail.com>
2025-11-25 22:55:23 +0300

    routh define

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import Home from "./pages/home/home.jsx";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/home/home';
+import About from './pages/about/about';
+// Import other pages as you create them
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/export-regions" element={<div>Export Regions Page</div>} />
+            <Route path="/products" element={<div>Products Page</div>} />
+            <Route path="/partners" element={<div>Partners Page</div>} />
+            <Route path="/how-we-work" element={<div>How We Work Page</div>} />
+            <Route path="/contact" element={<div>Contact Page</div>} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
