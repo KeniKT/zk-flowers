3389c59be06c6e3cbfb83d39f4ae742955b40d70
KT <keni232127@gmail.com>
2025-12-03 20:18:23 +0300

    routh update

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -5,12 +5,9 @@ import StorySection from "./components/StorySection.jsx";
 import MissionVisionValues from "./components/MissionVisionValues.jsx";
 import ProductionCapabilities from "./components/ProductionCapabilities.jsx";
 import MarketPresence from "./components/MarketPresence.jsx";
-import VarietiesSection from "./components/VarietiesSection.jsx";
 import CommunityImpact from "./components/CommunityImpact.jsx";
 import WhyChooseUs from "./components/WhyChooseUs.jsx";
-
 import CTASection from "./components/CTASection.jsx";
-
 export default function AboutPage() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-green-50 to-white">
@@ -19,10 +16,8 @@ export default function AboutPage() {
       <MissionVisionValues />
       <ProductionCapabilities />
       <MarketPresence />
-      <VarietiesSection />
       <CommunityImpact />
       <WhyChooseUs />
-   
       <CTASection />
     </div>
   );
