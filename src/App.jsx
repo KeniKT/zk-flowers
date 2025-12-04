7b9627442a1cee7e1756eeefd145ff3411a88a95
KT <keni232127@gmail.com>
2025-12-04 04:15:59 +0300

    scroll to top fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -7,11 +8,13 @@ import ExportRegions from './pages/export-regions/ExportRegions';
 import WhatWeOffer from './pages/products/WhatWeOffer';
 import OurPartners from './pages/partners/OurPartners';
 import HowWeWork from './pages/how-we-work/HowWeWork';
-import Contact from './pages/contact/Contact';  // ADD THIS LINE
+import Contact from './pages/contact/Contact';
+import ScrollToTop from './components/ScrollToTop'; // IMPORT ScrollToTop
 
 function App() {
   return (
     <Router>
+      <ScrollToTop /> {/* ADD ScrollToTop HERE */}
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
@@ -22,7 +25,7 @@ function App() {
             <Route path="/products" element={<WhatWeOffer />} />
             <Route path="/partners" element={<OurPartners />} />
             <Route path="/how-we-work" element={<HowWeWork />} />
-            <Route path="/contact" element={<Contact />} />  {/* ADD THIS LINE */}
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
         <Footer />
