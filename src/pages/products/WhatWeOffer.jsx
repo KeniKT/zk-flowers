8306e387c0953a8ee2e4a47417944fa4b05e3e9e
KT <keni232127@gmail.com>
2025-12-17 18:32:47 +0300

    product page one section remove

--- src/pages/products/WhatWeOffer.jsx
+++ src/pages/products/WhatWeOffer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
-import VarietiesSection from "./components/VarietiesSection.jsx";
 import RoseVarietiesSection from "./components/RoseVarietiesSection";
 import ServicesSection from "./components/ServicesSection";
 import QualityStandardsSection from "./components/QualityStandardsSection";
@@ -10,7 +9,6 @@ export default function WhatWeOffer() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-green-50 to-white">
       <HeroSection />
-       <VarietiesSection />
       <RoseVarietiesSection />
       <ServicesSection />
       <QualityStandardsSection />
